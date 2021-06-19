a73ee391e6a5ebfd63d085ec5d9dd6ce81c3180c
myrean <donaldrhughes@gmail.com>
2021-06-19 13:08:09 -0700

    inferrance

--- app.ts
+++ app.ts
@@ -9,7 +9,8 @@ const add2 = (n1:number, n2:number, showResult: boolean, phrase: string) => {
     }
 }
 
-const number1 = 5;
+let number1:number;
+number1 = 5;
 const number2 = 2.8;
 const printResult = true;
 const resultPhrase = "Result is: "
